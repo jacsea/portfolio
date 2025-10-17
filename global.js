fb2f3390ac0fe2352a3a4e9f6fd155b63766ef15
jacsea <164080379+jacsea@users.noreply.github.com>
2025-10-17 16:18:31 -0700

    Fixing repo name

--- global.js
+++ global.js
@@ -29,7 +29,7 @@ document.body.prepend(nav);
 const BASE_PATH =
   location.hostname === 'localhost' || location.hostname === '127.0.0.1'
     ? '/' // Local server
-    : '/portfolio/'; // GitHub Pages repo name
+    : 'jacsea.github.io'; // GitHub Pages repo name
 
 for (let p of pages) {
   let url = p.url;
