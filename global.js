733c1846dd1a75c8cc0fac670e4aa18f7a30a0fe
jacsea <164080379+jacsea@users.noreply.github.com>
2025-10-24 21:44:45 -0700

    Lab 4

--- global.js
+++ global.js
@@ -82,3 +82,36 @@ select.addEventListener('input', function (event) {
   localStorage.colorScheme = event.target.value
 });
 
+export async function fetchJSON(url) {
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+    console.log(response);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+export function renderProjects(project, containerElement, headingLevel = 'h2') {
+  // Your code will go here
+  containerElement.innerHTML = '';
+  for (let p of project){
+  const article = document.createElement('article');
+  article.innerHTML = `
+    <${headingLevel}>${p.title}</${headingLevel}>
+    <img src="${p.image}" alt="${p.title}">
+    <p>${p.description}</p>
+  `;
+  containerElement.appendChild(article);
+  };
+}
+
+export async function fetchGitHubData(username) {
+  // return statement here
+  return fetchJSON(`https://api.github.com/users/${username}`);
+}
